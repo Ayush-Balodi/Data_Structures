2ccdec9f2e2c56426cca285ea49f31a863ad3560
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2022-12-02 22:30:23 +0530

    min heap

---
+++ Sort/minHeap.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void swap( int *a , int *b ){
+    int temp = *a ;
+    *a = *b;
+    *b = temp;
+    return;    
+}
+
+void heapify( int nums[] , int n , int index ){
+    
+    int c1 = 2*index+1 , c2 = 2*index+2;
+    
+    if( c1<n && nums[c1] < nums[index] ){
+        swap( &nums[c1] , &nums[index] );
+        heapify(nums , n , c1);
+    }
+    if( c2<n && nums[c2] < nums[index] ){
+        swap( &nums[c2] , &nums[index] );
+        heapify(nums , n , c2);
+    }
+}
+
+void heapSort( int nums[] , int n ){
+    
+    for( int i=n/2 ; i>=0 ; i-- ){
+        heapify(nums , n , i);
+    }
+    return;
+}
+
+int main (){
+    
+    int nums[] = { 23 , 17 , 31 , 43 , 55 , 27 , 19 , 10 };
+    int n = sizeof(nums)/sizeof(nums[0]);
+    printf("Answer : ");
+    for( int i=0 ; i<n ; i++ ){
+        printf("%d ",nums[i]);
+    }
+    heapSort( nums, n);
+    printf("Answer : ");
+    for( int i=0 ; i<n ; i++ ){
+        printf("%d ",nums[i]);
+    }
+    printf("\n");
+    return 0;
+}
